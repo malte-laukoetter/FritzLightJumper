7f64ebb9242e6a48b35526b4e8d717bb2ada0e45
Tom Dy <tomdymel@web.de>
2017-01-10 13:09:37 +0100

    it is a small step for me but a huge step for us, or so

--- Main.hs
+++ Main.hs
@@ -4,21 +4,24 @@ import Network.MateLight.Simple
 import Data.Maybe
 import qualified Network.Socket as Sock
 
-move :: (Int, Int) -> String -> (Int, Int) -> (Int, Int)
-move (xdim, ydim) "\"j\"" (x, y) = (x, (y + 1) `mod` ydim)
-move (xdim, ydim) "\"k\"" (x, y) = (x, (y - 1) `mod` ydim)
-move (xdim, ydim) "\"h\"" (x, y) = ((x - 1) `mod` xdim, y)
-move (xdim, ydim) "\"l\"" (x, y) = ((x + 1) `mod` xdim, y)
-move _ _ x = x
-
-toFrame :: (Int, Int) -> (Int, Int) -> ListFrame
-toFrame (xdim, ydim) (x', y') = ListFrame $ map (\y -> map (\x -> if x == x' && y == y' then Pixel 0xff 0xff 0xff else Pixel 0 0 0) [0 .. xdim - 1]) [0 .. ydim - 1]
-
-eventTest :: [Event String] -> (Int, Int) -> (ListFrame, (Int, Int))
-eventTest events pixel = (toFrame dim pixel', pixel')
-  where pixel' = foldl (\acc (Event mod ev) -> if mod == "KEYBOARD" then move dim ev acc else acc) pixel events
-
 dim :: (Int, Int)
 dim = (30, 12)
+
+type Pos = (Int, Int)
+type Area = (Pos, Pos)
+type Canvas = [[Pixel]]
+
+inArea :: Area -> Pos -> Bool
+inArea ((x1, y1), (x2, y2)) (x, y) = x>=x1 && x<=x2 && y>=y1 && y<=y2
+
+inAreas :: [Area] -> Pos -> Bool
+inAreas xs p = filter ((flip inArea) p) xs /= []
+
+drawCanvas :: [Area] -> Canvas
+drawCanvas areas = [[if inAreas areas (x, y) then Pixel 255 0 0 else Pixel 0 0 0 | x <- [0..(fst dim)-1]] | y <- [0..(snd dim)-1]]
+
+toFrame :: [Event String] -> [Area] -> (ListFrame, [Area])
+toFrame _ a = (ListFrame (drawCanvas a), a)
+
 main :: IO ()
-main = Sock.withSocketsDo $ runMate (Config (fromJust $ parseAddress "134.28.70.172") 1337 dim (Just 500000) False []) eventTest (0, 0)
+main = Sock.withSocketsDo $ runMate (Config (fromJust $ parseAddress "127.0.0.1") 1337 dim (Just 33000) False []) toFrame [((5,5), (10,10)), ((15,5), (20,10))]
