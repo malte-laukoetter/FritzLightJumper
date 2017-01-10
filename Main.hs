7b8f45eae8eb4787c605a5a71b7053c29d3ae885
Tom Dy <tomdymel@web.de>
2017-01-10 16:14:59 +0100

    Endless scrolling generation

--- Main.hs
+++ Main.hs
@@ -19,9 +19,12 @@ inArea ((x1, y1), (x2, y2)) (x, y) = x>=x1 && x<=x2 && y>=y1 && y<=y2
 inAreas :: [Area] -> Pos -> Bool
 inAreas xs p = filter ((flip inArea) p) xs /= []
 
+getScrollAlpha :: Time -> Int
+getScrollAlpha time = floor (fromIntegral time/2)
+
 drawCanvas :: [Area] -> Time -> Canvas
 drawCanvas areas time = [[if inAreas areas (x+aX, y) then Pixel 255 0 0 else Pixel 0 0 0 | x <- [0..(fst dim)-1]] | y <- [0..(snd dim)-1]]
-  where aX = floor (fromIntegral time/2);
+  where aX = getScrollAlpha time
 
 -- Now we can basically build a small level
 level :: [Area]
@@ -34,12 +37,16 @@ level = [
   ((50, 9), (53,12))]
 
 toFrame :: [Event String] -> ([Area], Time) -> (ListFrame, ([Area], Time))
-toFrame _ (a, b) = (ListFrame (drawCanvas a (b+1)), (a, b+1))
+toFrame _ (a, b) = (ListFrame (drawCanvas vA (b+1)), (vA, b+1))
+  where vA = genVisAreas (b+1)
 
 -- We will probably build a whole new module out of this
+-- 3 Elements visible at a time?
 genVisAreas :: Time -> [Area]
-genVisAreas time = undefined
+genVisAreas time = [((aX, 11), (30+aX, 12))] ++ [((x*10+5, 9), (x*10+8, 12)) | x <- [cX-2..cX+5]]
+  where aX = getScrollAlpha time
+        cX = floor (fromIntegral aX/10)
 
 main :: IO ()
 --main = Sock.withSocketsDo $ runMate (Config (fromJust $ parseAddress "127.0.0.1") 1337 dim (Just 33000) False []) toFrame (level, 0)
-main = Sock.withSocketsDo $ runMate (Config (fromJust $ parseAddress "134.28.70.172") 1337 dim (Just 33000) False []) toFrame (level, 0)
+main = Sock.withSocketsDo $ runMate (Config (fromJust $ parseAddress "134.28.70.172") 1337 dim (Just 33000) False []) toFrame (genVisAreas 0, 0)
