50bff4dd16c3af1079e4fcab7eb3377c14fd7646
Tom Dy <tomdymel@web.de>
2017-01-10 17:16:50 +0100

    Random level generation

--- Main.hs
+++ Main.hs
@@ -13,6 +13,13 @@ type Pos = (Int, Int)
 type Area = (Pos, Pos)
 type Canvas = [[Pixel]]
 
+
+{-
+- Utility functions
+-}
+utiDiv :: Int -> Float -> Float
+utiDiv a b = (fromIntegral a) / b
+
 inArea :: Area -> Pos -> Bool
 inArea ((x1, y1), (x2, y2)) (x, y) = x>=x1 && x<=x2 && y>=y1 && y<=y2
 
@@ -20,7 +27,7 @@ inAreas :: [Area] -> Pos -> Bool
 inAreas xs p = filter ((flip inArea) p) xs /= []
 
 getScrollAlpha :: Time -> Int
-getScrollAlpha time = floor (fromIntegral time/2)
+getScrollAlpha time = floor $ utiDiv time 2
 
 drawCanvas :: [Area] -> Time -> Canvas
 drawCanvas areas time = [[if inAreas areas (x+aX, y) then Pixel 255 0 0 else Pixel 0 0 0 | x <- [0..(fst dim)-1]] | y <- [0..(snd dim)-1]]
@@ -43,9 +50,19 @@ toFrame _ (a, b) = (ListFrame (drawCanvas vA (b+1)), (vA, b+1))
 -- We will probably build a whole new module out of this
 -- 3 Elements visible at a time?
 genVisAreas :: Time -> [Area]
-genVisAreas time = [((aX, 11), (30+aX, 12))] ++ [((x*10+5, 9), (x*10+8, 12)) | x <- [cX-2..cX+5]]
-  where aX = getScrollAlpha time
-        cX = floor (fromIntegral aX/10)
+genVisAreas time = bottom ++ [((xPos x, yPos x - size), (xPos x + size, yPos x)) | x <- [cX-2..cX+5]]
+  where aX      = getScrollAlpha time
+        cX      = floor $ utiDiv aX 10
+        maxGap  = 2
+        size    = 2
+        gap x'  = (x' `mod` ((x' `mod` maxGap)+1))
+        xPos x' = x'*10 + (gap x') + 5
+        yPos x' | ((sin (utiDiv (2*x') 2.34))-0.5) > 0.5 = 10-size
+                | ((sin (utiDiv (2*x') 2.34))-0.5) > 0   = 12-size
+                | otherwise                              = 12
+        bottom = [((0, 11), (150, 12))]
+        --bottom = []
+        --bottom = [((x', 11), (x',12)) | x' <- [aX..fst dim-1+aX], (sin $ utiDiv x' 10) > 0]
 
 main :: IO ()
 --main = Sock.withSocketsDo $ runMate (Config (fromJust $ parseAddress "127.0.0.1") 1337 dim (Just 33000) False []) toFrame (level, 0)
