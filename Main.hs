020327a85813bd212aef60a30d66e04bbb0dfc7e
Malte <lergin@gmx.de>
2017-01-10 12:59:14 +0100

    some tests

--- Main.hs
+++ Main.hs
@@ -4,6 +4,15 @@ import Network.MateLight.Simple
 import Data.Maybe
 import qualified Network.Socket as Sock
 
+type Pos = (Int, Int)
+type Area = (Pos, Pos)
+
+inArea :: Area -> Pos -> Bool
+inArea ((x1, x2), (y1, y2)) (x, y) = x >= x1 && x <= x2 && y >= y1 && y <= y2
+
+drawArea :: Area -> [[Pixel]]
+drawArea a = map (\y -> map (\x -> if inArea a (x, y) then Pixel 0xff 0 0xff else Pixel 0 0 0) [0 .. (fst dim) - 1]) [0 .. (snd dim) - 1]
+
 move :: (Int, Int) -> String -> (Int, Int) -> (Int, Int)
 move (xdim, ydim) "\"j\"" (x, y) = (x, (y + 1) `mod` ydim)
 move (xdim, ydim) "\"k\"" (x, y) = (x, (y - 1) `mod` ydim)
@@ -12,7 +21,7 @@ move (xdim, ydim) "\"l\"" (x, y) = ((x + 1) `mod` xdim, y)
 move _ _ x = x
 
 toFrame :: (Int, Int) -> (Int, Int) -> ListFrame
-toFrame (xdim, ydim) (x', y') = ListFrame $ map (\y -> map (\x -> if x == x' && y == y' then Pixel 0xff 0xff 0xff else Pixel 0 0 0) [0 .. xdim - 1]) [0 .. ydim - 1]
+toFrame (xdim, ydim) (x', y') = ListFrame (drawArea ((1, 5), (2, 10)))
 
 eventTest :: [Event String] -> (Int, Int) -> (ListFrame, (Int, Int))
 eventTest events pixel = (toFrame dim pixel', pixel')
@@ -21,4 +30,4 @@ eventTest events pixel = (toFrame dim pixel', pixel')
 dim :: (Int, Int)
 dim = (30, 12)
 main :: IO ()
-main = Sock.withSocketsDo $ runMate (Config (fromJust $ parseAddress "134.28.70.172") 1337 dim (Just 500000) False []) eventTest (0, 0)
+main = Sock.withSocketsDo $ runMate (Config (fromJust $ parseAddress "127.0.0.1") 1337 dim (Just 33000) False []) eventTest (0, 0)
