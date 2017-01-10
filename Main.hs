63587c6a8196197b45025af265b90064b4a79434
Tom Dy <tomdymel@web.de>
2017-01-10 15:35:27 +0100

    Enabling to build little level

--- Main.hs
+++ Main.hs
@@ -7,6 +7,8 @@ import qualified Network.Socket as Sock
 dim :: (Int, Int)
 dim = (30, 12)
 
+type Time = Int
+
 type Pos = (Int, Int)
 type Area = (Pos, Pos)
 type Canvas = [[Pixel]]
@@ -17,11 +19,27 @@ inArea ((x1, y1), (x2, y2)) (x, y) = x>=x1 && x<=x2 && y>=y1 && y<=y2
 inAreas :: [Area] -> Pos -> Bool
 inAreas xs p = filter ((flip inArea) p) xs /= []
 
-drawCanvas :: [Area] -> Canvas
-drawCanvas areas = [[if inAreas areas (x, y) then Pixel 255 0 0 else Pixel 0 0 0 | x <- [0..(fst dim)-1]] | y <- [0..(snd dim)-1]]
+drawCanvas :: [Area] -> Time -> Canvas
+drawCanvas areas time = [[if inAreas areas (x+aX, y) then Pixel 255 0 0 else Pixel 0 0 0 | x <- [0..(fst dim)-1]] | y <- [0..(snd dim)-1]]
+  where aX = floor (fromIntegral time/2);
 
-toFrame :: [Event String] -> [Area] -> (ListFrame, [Area])
-toFrame _ a = (ListFrame (drawCanvas a), a)
+-- Now we can basically build a small level
+level :: [Area]
+level = [
+  ((0, 11), (10000000, 12)),
+  ((10, 9), (13,12)),
+  ((20, 9), (23,12)),
+  ((30, 9), (33,12)),
+  ((40, 9), (43,12)),
+  ((50, 9), (53,12))]
+
+toFrame :: [Event String] -> ([Area], Time) -> (ListFrame, ([Area], Time))
+toFrame _ (a, b) = (ListFrame (drawCanvas a (b+1)), (a, b+1))
+
+-- We will probably build a whole new module out of this
+genVisAreas :: Time -> [Area]
+genVisAreas time = undefined
 
 main :: IO ()
-main = Sock.withSocketsDo $ runMate (Config (fromJust $ parseAddress "127.0.0.1") 1337 dim (Just 33000) False []) toFrame [((5,5), (10,10)), ((15,5), (20,10))]
+--main = Sock.withSocketsDo $ runMate (Config (fromJust $ parseAddress "127.0.0.1") 1337 dim (Just 33000) False []) toFrame (level, 0)
+main = Sock.withSocketsDo $ runMate (Config (fromJust $ parseAddress "134.28.70.172") 1337 dim (Just 33000) False []) toFrame (level, 0)
